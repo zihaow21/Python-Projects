6393928ca0c82006023b6fbcb4f8e72a645db012
ZW <zihaow21@gmail.com>
2017-01-25 08:22:21 -0500

    change

--- Tasks/Alexa_Project/redditreader.py
+++ Tasks/Alexa_Project/redditreader.py
@@ -7,4 +7,3 @@ import unidecode
 
 app = Flask(__name__)
 ask = Ask(app, "/reddit_reader")
-Command "/home/zwan438/testnuc/bin/python2.7 -u -c "import setuptools, tokenize;__file__='/tmp/pip-build-dADNh3/cryptography/setup.py';f=getattr(tokenize, 'open', open)(__file__);code=f.read().replace('\r\n', '\n');f.close();exec(compile(code, __file__, 'exec'))" install --record /tmp/pip-q4f9nX-record/install-record.txt --single-version-externally-managed --compile --install-headers /home/zwan438/testnuc/include/site/python2.7/cryptography" failed with error code 1 in /tmp/pip-build-dADNh3/cryptography/
