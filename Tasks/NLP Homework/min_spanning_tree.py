7ba5963d011af2364ef1bc6d1899ccb054afd1f5
zihaow21 <zihaow21@gmail.com>
2017-01-30 15:48:34 -0500

    change

--- Tasks/NLP Homework/min_spanning_tree.py
+++ Tasks/NLP Homework/min_spanning_tree.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import graphviz as gz
 
 file = "/Users/ZW/Dropbox/current/Courses/2016 NLP Homework/Homework2/word_vectors.txt"
 
@@ -7,4 +8,5 @@ with open(file, 'r') as f:
     for line in f:
         t = line.split('\t')
         data[t[0]] = t[1:]
+
 
