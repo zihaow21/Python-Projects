8b50fc35253b18f0de9950672a4a26b5e4215509
ZW <zihaow21@gmail.com>
2017-01-30 23:25:21 -0500

    change

--- Tasks/NLP Homework/min_spanning_tree.py
+++ Tasks/NLP Homework/min_spanning_tree.py
@@ -1,11 +1,13 @@
 from __future__ import division
-import networkx as nx
 import graphviz as gz
+import networkx as nx
 import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-# file = "/Users/ZW/Dropbox/current/Courses/2016 NLP Homework/Homework2/word_vectors.txt"
-file = "/home/zwan438/temp_folder/HomeworkFiles/word_vectors.txt"
+file = "/Users/ZW/Dropbox/current/Courses/2016 NLP Homework/Homework2/word_vectors.txt"
+# file = "/home/zwan438/temp_folder/HomeworkFiles/word_vectors.txt"
 
 with open(file, 'r') as f:
     data = []
@@ -33,12 +35,18 @@ final_list = []
 for item1 in data:
     S[item1[0]] = []
     visited.append(item1)
-    for item2 in data:
+    for i, item2 in enumerate(data):
         if item2 not in visited:
-            S[item1[0]].append(cosines(item1[1], item2[1]))
-    list_compare = S[item1[0]]
-    if list_compare != []:
-        id = S[item1[0]].index(max(list_compare))
-        final_list.append([item1[0], data[id][0], S[item1[0]][id]])
+            S[item1[0]].append([i, cosines(item1[1], item2[1])])
+    if S[item1[0]] != []:
+        list_compare = list(np.array(S[item1[0]])[:, 1])
+        id = list_compare.index(max(list_compare))
+        final_list.append([item1[0], data[S[item1[0]][id][0]][0], S[item1[0]][id][1]])
 
-print final_list
+with open("/Users/ZW/Dropbox/current/Courses/2016 NLP Homework/Homework2/sample.dot", "wb") as fout:
+# with open("/home/zwan438/temp_folder/HomeworkFiles/sample.dot", "wb") as fout:
+    fout.write('digraph G {\n')
+    for item in final_list:
+        fout.write('"{}" -> "{}" [label="{}"];\n'.format(item[0], item[1], item[2]))
+    fout.write('}\n')
+
