14a7bee5eb49fd957ac4075df875e8cb8b9748d6
zihaow21 <zihaow21@gmail.com>
2017-02-09 09:13:08 -0500

    commit

--- Tasks/Alexa_Project/chitchat_conversation.py
+++ Tasks/Alexa_Project/chitchat_conversation.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_ask import Ask, statement, question
-from Tasks.Chatbot_Practice
+from Tasks.Chatbot_Practice.chitchat_component import ChitChat
 import json
 import requests
 import unidecode
@@ -9,6 +9,29 @@ import unidecode
 app = Flask(__name__)
 ask = Ask(app, "/chitchat")
 
-def chitchat():
+def chitchat(query):
+    cc = ChitChat()
+    response = cc.mainTest(query)
+    return response
 
-    
+@app.route('/')
+def homepage():
+    return "Hi, there how are you?"
+
+@ask.launch
+def start_skill():
+    welcome_message = "Hello there, what's up?"
+    return question(welcome_message)
+
+@ask.intent("ContinueIntent")
+def conversation(query):
+    response = chitchat(query)
+    return statement(response)
+
+@ask.intent("ExitIntent")
+def exit_intent():
+    exit_message = "It was greating talking to you. Have a great one!"
+    return statement(exit_message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
