0686a37b09401ce631af13c97f8e1767659a591a
ZW <zihaow21@gmail.com>
2017-02-12 09:48:29 -0500

    commit

--- Tasks/NLP Homework/perceptron.py
+++ Tasks/NLP Homework/perceptron.py
@@ -11,9 +11,10 @@ f_trn = "/Users/ZW/Dropbox/current/Courses/2016 NLP Homework/Homework3/sst.trn.t
 f_dev = "/Users/ZW/Dropbox/current/Courses/2016 NLP Homework/Homework3/sst.dev.tsv"
 f_tst = "/Users/ZW/Dropbox/current/Courses/2016 NLP Homework/Homework3/sst.tst.tsv"
 
-model_dir = "/Users/ZW/Dropbox/current/Courses/2016 NLP Homework/Homework3/perceptron.pik"
+model_dir_tfidf = "/Users/ZW/Dropbox/current/Courses/2016 NLP Homework/Homework3/perceptron_tfidf.pik"
+model_dir_onehot = "/Users/ZW/Dropbox/current/Courses/2016 NLP Homework/Homework3/perceptron_onehot.pik"
 
-
+#  tfidf method
 def tfidf(f_input):
     term_f = []
     vocabulary = []
@@ -46,7 +47,7 @@ def tfidf(f_input):
     return tf_idf, vocabulary
 
 
-def vector(base, tf_idf):
+def tfidf_vector(base, tf_idf):
     vec_doc = []
     for ele in tf_idf:
         vec = len(base) * [0]
@@ -58,16 +59,52 @@ def vector(base, tf_idf):
 
     return vec_doc
 
+
+# oneHot method
+def oneHot_vocab(f_input):
+    vocab = []
+    doc_tuple = []
+    with open(f_input, "rb") as f:
+        for line in f:
+            value = line.split("\t")
+            label = int(value[0])
+            doc = list(set(value[1].split()))
+            doc_tuple.append((label, doc))
+            vocab.extend(doc)
+    vocabulary = sorted(list(set(vocab)))
+
+    return vocabulary, doc_tuple
+
+def onehot_vector(base, doc_tuple):
+    onehot_vec = []
+    for label, vocab in doc_tuple:
+        vec_temp = [0] * len(base)
+        for v in vocab:
+            vec_temp[base.index(v)] = 1
+        onehot_vec.append((label, vec_temp))
+
+    return onehot_vec
+
+#  tfidf method
 tf_idf_trn, vocab_trn = tfidf(f_trn)
 tf_idf_dev, vocab_dev = tfidf(f_dev)
 tf_idf_tst, vocab_tst = tfidf(f_tst)
 
 vocab = sorted(list(set(list(vocab_trn) + list(vocab_dev) + list(vocab_tst))))
 vec_len = len(vocab)
-vec_trn = vector(vocab, tf_idf_trn)
-vec_dev = vector(vocab, tf_idf_dev)
-vec_tst = vector(vocab, tf_idf_tst)
+vec_trn = tfidf_vector(vocab, tf_idf_trn)
+vec_dev = tfidf_vector(vocab, tf_idf_dev)
+vec_tst = tfidf_vector(vocab, tf_idf_tst)
 
+#  onehot method
+vocabulary_trn, doc_tuple_trn = oneHot_vocab(f_trn)
+vocabulary_dev, doc_tuple_dev = oneHot_vocab(f_dev)
+vocabulary_tst, doc_tuple_tst = oneHot_vocab(f_tst)
+
+vocabulary = sorted(list(set(list(vocabulary_trn) + list(vocabulary_dev) + list(vocabulary_tst))))
+vector_trn = onehot_vector(vocabulary, doc_tuple_trn)
+vector_dev = onehot_vector(vocabulary, doc_tuple_dev)
+vector_tst = onehot_vector(vocabulary, doc_tuple_tst)
 
 class MultiClassPerceptron(object):
     def __init__(self, classes, data_train, epochs, model_dir):
@@ -107,22 +144,24 @@ class MultiClassPerceptron(object):
 
             accuracy = count/len(class_pair)
 
-            if k % 30 == 0:
+            if k % 5 == 0:
                 print "The training accuracy for epoch {} is {}".format(k, accuracy)
 
     def saveModel(self):
-        with open(model_dir, "wb") as f:
+        # with open(model_dir_tfidf, "wb") as f:
+        with open(model_dir_onehot, "wb") as f:
             pickle.dump(self, f, pickle.HIGHEST_PROTOCOL)
 
     def loadModel(self):
-        with open(self.model_dir, "rb") as f:
+        # with open(self.model_dir, "rb") as f:
+        with open(model_dir_onehot, "rb") as f:
             return pickle.load(f)
 
     def pred(self, data_dev):
         class_pair = []
         count = 0
-        for label, data in data_dev:
-            data = np.array(data.append(1))
+        for label, vec in data_dev:
+            data = np.array(vec + [1.0])
 
             argmax, prediction = 0, self.classes[0]
 
@@ -139,10 +178,12 @@ class MultiClassPerceptron(object):
 
         accuracy = count/len(class_pair)
 
-        return accuracy
+        print accuracy
 
-mcp = MultiClassPerceptron([0, 1, 2, 3, 4], vec_trn, 100, model_dir)
+# mcp = MultiClassPerceptron([0, 1, 2, 3, 4], vec_trn, 15, model_dir_tfidf)
+mcp = MultiClassPerceptron([0, 1, 2, 3, 4], vector_trn, 33, model_dir_onehot)
 mcp.train()
 mcp.saveModel()
 mcp.loadModel()
-mcp.pred(vec_dev)
+# mcp.pred(vec_dev)
+mcp.pred(vector_dev)
