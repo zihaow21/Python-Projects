944bb5fbb86103d1f87579449c3928ad0dd85a51
ZW <zihaow21@gmail.com>
2017-02-15 01:38:47 -0500

    commit

--- Tasks/Alexa_Project/redditreader.py
+++ Tasks/Alexa_Project/redditreader.py
@@ -5,45 +5,42 @@ import requests
 import unidecode
 
 app = Flask(__name__) # a typical app definition in flask
-ask = Ask(app, "/reddit_reader")  #  ask app, part of the flask app; /: end point, where the ask app would be in the file system
+ask = Ask(app, "/")  #  ask app, part of the flask app; /: end point, where the ask app would be in the file system
 
-def get_headlines():
-    user_pass_dict = {'user': 'zihaow21', 'passwd': 'Wzh131313+', 'api_type': 'json'}  # user login info
 
-    sess = requests.Session()  # start a session
-    sess.headers.update({'User-Agent': 'I am testing Alexa'})
-    sess.post('https://www.reddit.com/api/login', data=user_pass_dict)  # pass the credentials
+# def get_headlines():
+#     user_pass_dict = {'user': 'zihaow21', 'passwd': 'Wzh131313+', 'api_type': 'json'}  # user login info
+#
+#     sess = requests.Session()  # start a session
+#     sess.headers.update({'User-Agent': 'I am testing Alexa'})
+#     sess.post('https://www.reddit.com/api/login', data=user_pass_dict)  # pass the credentials
+#
+#     # api format
+#     url = 'https://reddit.com/r/worldnews/.json?limit=10'
+#     html = sess.get(url)
+#     data = json.loads(html.content.decode('utf-8'))
+#
+#     # data processing
+#     titles = [unidecode.unidecode(listing['data']['title']) for listing in data['data']['children']]
+#     titles = '... '.join([i for i in titles])
+#
+#     return titles
 
-    # api format
-    url = 'https://reddit.com/r/worldnews/.json?limit=10'
-    html = sess.get(url)
-    data = json.loads(html.content.decode('utf-8'))
-
-    # data processing
-    titles = [unidecode.unidecode(listing['data']['title']) for listing in data['data']['children']]
-    titles = '... '.join([i for i in titles])
-
-    return titles
-
-@app.route('/') # sets the path of the url
-def homepage():
-    return "hi there, how you are doing?"
 
 @ask.launch
-def start_skill():
-    welcome_message = "Hello there, would you like the news?"
+def launched():
+    welcome_message = "Hello there?"
     return question(welcome_message)
 
-@ask.intent("YesIntent")
-def share_headlines():
-    headlines = get_headlines()
-    headline_msg = "The current world news headlines are {}".format(headlines)
+
+@ask.intent("UserInput", default={'sentence': 'hello'})
+def share_headlines(sentence):
+
+    # headlines = get_headlines()
+    # headline_msg = "The current world news headlines are {}".format(headlines)
+    headline_msg = "The message input by user is {}".format(sentence)
     return statement(headline_msg)
 
-@ask.intent("NoIntent")
-def no_intent():
-    bye_text = 'I am not sure why you asked me to run then, but okay ... bye'
-    return statement(bye_text)
 
 if __name__ == '__main__': # run the app
     app.run(debug=True)
--- Tasks/WashingtonPost/craw_data.py
+++ Tasks/WashingtonPost/craw_data.py
@@ -1,9 +1,10 @@
 import requests
 import json
+import pymongo
 
 
-start_date = "2017/02/12"
-end_date = "2017/02/13"
+start_date = "2016/02/11"
+end_date = "2016/02/12"
 count = "10"
 
 News_API = "https://docs.washpost.com/docs?stdate=" + start_date + "&enddate=" + end_date + "&offset=0&count=" + count + "&key=zgi53MiWSM9FbsYAPrvz"
@@ -16,14 +17,23 @@ news_data = json.loads(response_news.content.decode('ascii'))
 comments_data = json.loads(response_comments.content.decode('ascii'))
 
 keys = ["contenttype", "associatedbinaryurl"]
-for item1, item2 in zip(news_data["docs"], comments_data["docs"]):
+for item1, item2 in zip(news_data['docs'], comments_data['docs']):
     for key in keys:
         item1.pop(key, None)
 
     item1["comments"] = item2["comments"]
+    item1["date"] = start_date
+
+
+connection = pymongo.MongoClient("mongodb://localhost")
+db = connection.washington_news
+record = db.collection
+
+for item in news_data["docs"]:
+    record.insert(item)
 
 # with open("/Users/ZW/dropbox/WashingtonPostData/NewsSample.txt", "w") as f:
-with open("/home/zwan438/WashingtonPost/NewsSample.txt", "w") as f:
-    json.dump(news_data, f)
-    # print >> f, news_data
+# # with open("/home/zwan438/WashingtonPost/NewsSample.txt", "w") as f:
+#     json.dump(news_data, f)
+#     # print >> f, news_data
 
