0139d97e57d528b5ef30c352cad07fb9850a99c7
zihaow21 <zihaow21@gmail.com>
2017-03-04 14:59:16 -0500

    commit

--- Tasks/Chatbot/ChitChat/Preprocessing/create_conversation.py
+++ Tasks/Chatbot/ChitChat/Preprocessing/create_conversation.py
@@ -5,16 +5,16 @@ from tqdm import tqdm
 
 
 # movie_lines_filename = '/Users/ZW/Dropbox/data/cornell movie-dialogs corpus/movie_lines.txt'
-movie_lines_filename = '/home/zwan438/Dropbox/data/cornell movie-dialogs corpus/movie_lines.txt'
-# movie_lines_filename = '/home/zihao/temp_folder/movie_lines.txt'
+# movie_lines_filename = '/home/zwan438/Dropbox/data/cornell movie-dialogs corpus/movie_lines.txt'
+movie_lines_filename = '/home/zihao/temp_folder/movie_lines.txt'
 
 # movie_conversations_filename = '/Users/ZW/Dropbox/data/cornell movie-dialogs corpus/movie_conversations.txt'
-movie_conversations_filename = '/home/zwan438/Dropbox/data/cornell movie-dialogs corpus/movie_conversations.txt'
-# movie_conversations_filename = '/home/zihao/temp_folder/movie_conversations.txt'
+# movie_conversations_filename = '/home/zwan438/Dropbox/data/cornell movie-dialogs corpus/movie_conversations.txt'
+movie_conversations_filename = '/home/zihao/temp_folder/movie_conversations.txt'
 
 # data_conversation_dir = '/Users/ZW/Dropbox/Current/temp/chitchat_conversation_data.txt'
-data_conversation_dir = '/home/zwan438/Dropbox/data/chitchat_conversation_data.txt'
-# data_conversation_dir = '/home/zihao/temp_folder/chitchat_conversation_data.txt'
+# data_conversation_dir = '/home/zwan438/Dropbox/data/chitchat_conversation_data.txt'
+data_conversation_dir = '/home/zihao/temp_folder/chitchat_conversation_data.txt'
 
 cd = CornellData(movie_lines_filename, movie_conversations_filename)
 conversations = cd.getConversations()
